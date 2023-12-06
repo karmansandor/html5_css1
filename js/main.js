040ad25614ec5d1468b1da63834a6bf96cbca580
Kármán Sándor <karman.sandor@infohullam.hu>
2023-12-06 23:59:00 +0100

    első mentés

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//Elemek kiválasztása
+// @param selector: string - az elemet kiválasztó szelektor
+function _q( selector ) {
+    //Elemek kiválasztása
+    var elements = document.querySelectorAll( selector );
+    
+    return elements;
+    
+}
